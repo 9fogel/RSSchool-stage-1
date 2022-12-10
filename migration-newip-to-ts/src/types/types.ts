3508bd017f9bee508bf5587f7598e87f950130e8
Vera Kirayonak <fogel_ver@mail.ru>
2022-12-11 01:20:31 +0300

    feat: create interfaces & types in src/types/types.ts

--- migration-newip-to-ts/src/types/types.ts
+++ migration-newip-to-ts/src/types/types.ts
@@ -1,2 +1,18 @@
-//https://newsapi.org/docs/endpoints/sources
-//https://newsapi.org/docs/endpoints/top-headlines
+export interface IResponse {
+  status: string;
+  totalResults: number;
+  articles: Array<IArticle>;
+}
+
+export interface IArticle {
+  source: SourceT;
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+}
+
+export type SourceT = { id: string; name: string };
