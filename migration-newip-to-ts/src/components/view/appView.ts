c95fbd4b144a1717dc06d01dc3e245164c952f86
Vera Kirayonak <fogel_ver@mail.ru>
2022-12-11 01:54:34 +0300

    refactor: import interfaces, add TS types to appView.ts

--- migration-newip-to-ts/src/components/view/appView.ts
+++ migration-newip-to-ts/src/components/view/appView.ts
@@ -1,19 +1,24 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import { IArticle, IResponse, ISourceRes, ISourcesData } from '../../types/types';
 
 export class AppView {
+  news: News;
+  sources: Sources;
   constructor() {
     this.news = new News();
     this.sources = new Sources();
   }
 
-  drawNews(data) {
-    const values = data?.articles ? data?.articles : [];
+  drawNews(data: IResponse | undefined): void {
+    // console.log('appView data', data);
+    const values: Array<IArticle> = data?.articles ? data?.articles : [];
     this.news.draw(values);
   }
 
-  drawSources(data) {
-    const values = data?.sources ? data?.sources : [];
+  drawSources(data: ISourceRes | undefined) {
+    // console.log('appView Sources', data);
+    const values: Array<ISourcesData> = data?.sources ? data?.sources : [];
     this.sources.draw(values);
   }
 }
