d470f5f7584809f707f409e5cb6f0438ca2dc825
Vera Kirayonak <fogel_ver@mail.ru>
2022-12-11 02:42:13 +0300

    refactor: import interfaces, , add TS types to app.ts

--- migration-newip-to-ts/src/components/app/app.ts
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -1,17 +1,25 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import { ISourceRes, IResponse } from '../../types/types';
 
 class App {
+  controller: AppController;
+  view: AppView;
   constructor() {
     this.controller = new AppController();
     this.view = new AppView();
   }
 
-  start() {
-    document
-      .querySelector('.sources')
-      .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-    this.controller.getSources((data) => this.view.drawSources(data));
+  start(): void {
+    const sources: HTMLElement | null = document.querySelector('.sources');
+    // console.log('app start', data);
+
+    if (sources) {
+      sources.addEventListener('click', (e: Event): void =>
+        this.controller.getNews(e, (data: IResponse | undefined) => this.view.drawNews(data)),
+      );
+      this.controller.getSources((data: ISourceRes | undefined) => this.view.drawSources(data));
+    }
   }
 }
 
