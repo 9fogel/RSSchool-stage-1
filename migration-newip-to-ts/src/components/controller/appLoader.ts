0c4354000ecf420be72583cc0e0eb1c797041eb1
Vera Kirayonak <fogel_ver@mail.ru>
2022-12-12 16:34:10 +0300

    fix: replace original API link with proxy link

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://nodenews.up.railway.app/', {
+      // super('https://newsapi.org/v2/', {
       // apiKey: '6fbcbe4526184d7f80bb2de2338d1d84', // получите свой ключ https://newsapi.org/
       apiKey: 'b046409c36214d2497ac233ba33b75cf',
     });
